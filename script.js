26cc9ba263a380834fd4fb756cc723cffba8dd65
Ricardo Lobo <ricardo@agendoscience.com>
2023-03-19 02:07:52 +0000

    Update button components

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+const addBook = document.getElementById("AddBook");
 let myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read) {
   // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
+
+Book.prototype.getInfo = function () {
+  const readStr = this.read ? "completed" : "not read yet";
+
+  return `${this.title} by ${this.author}, ${this.pages}, ${readStr}.`;
+};
 
 function addBookToLibrary() {
   // do stuff here
 }
+
+addBook.addEventListener("click", (e) => {});
